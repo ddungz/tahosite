5f36bef099cb1f04997534afb2fcaff390d0ff37
Dung Dam Duy <duydung1609@gmail.com>
2015-09-05 16:25:31 +0700

    fix

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2015-08-29 using generator-angular 0.12.1
+// Generated on 2015-09-05 using generator-angular 0.12.1
 'use strict';
 
 // # Globbing
@@ -319,7 +319,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'tahosite',
+          module: 'frontendApp',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('tahosite', [
+  .module('frontendApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
